3555e33f59fb1ab5426bd4787717b23307c5cb43
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-08-01 05:32:31 +0200

    test: validator middleware tested, full coverage

--- src/tests/unit/middleware.spec.ts
+++ src/tests/unit/middleware.spec.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import notFound from '../../middleware/notFound';
 import logger, { LoggerStream } from '../../middleware/winston';
+import statusCodes from '../../constants/statusCodes';
+import validator from '../../middleware/validator';
 
 describe('Middleware', () => {
   describe('notFound', () => {
@@ -20,6 +22,7 @@ describe('Middleware', () => {
       });
     });
   });
+
   describe('winston logger', () => {
     it('should log error message', () => {
       const error = new Error('Database error');
@@ -54,6 +57,65 @@ describe('Middleware', () => {
 
         expect(logger.info).toHaveBeenCalledWith('Test log message');
       });
+    });
+  });
+
+  describe('Validator', () => {
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let next: NextFunction;
+
+    beforeEach(() => {
+      req = {
+        body: {},
+      };
+      res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      next = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should delete creation_date if it exists in the request body', () => {
+      req.body = { creation_date: '2023-01-01', someKey: 'someValue' };
+
+      validator(req as Request, res as Response, next);
+
+      expect(req.body.creation_date).toBe(new Date().toJSON().slice(0, 10));
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should add current date as creation_date in the request body', () => {
+      req.body = { someKey: 'someValue' };
+
+      validator(req as Request, res as Response, next);
+
+      expect(req.body.creation_date).toBe(new Date().toJSON().slice(0, 10));
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should set empty string values to null', () => {
+      req.body = { someKey: '', anotherKey: 'value' };
+
+      validator(req as Request, res as Response, next);
+
+      expect(req.body.someKey).toBeNull();
+      expect(req.body.anotherKey).toBe('value');
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should handle errors and respond with a 400 status code', () => {
+      req.body = null; // This will throw an error in the for loop
+
+      validator(req as Request, res as Response, next);
+
+      expect(logger.error).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(statusCodes.badRequest);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Bad request' });
     });
   });
 });
