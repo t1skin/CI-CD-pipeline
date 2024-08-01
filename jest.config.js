c5c35a9578edbaa94ddf6aa39b8605a2910550f5
Khanh <hoangcaokhanhhn2004@gmail.com>
2024-08-01 23:04:50 +0700

    feat: added functionality for ignoring dist in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
